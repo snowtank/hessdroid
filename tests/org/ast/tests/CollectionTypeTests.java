216bed7a7441e092486bf30545c56c2b63f3382b
wolf@120.200.168.192.in-addr.arpa <wolf@120.200.168.192.in-addr.arpa>
2009-11-29 14:33:17 -0800

    Use autoboxing and fix annotations

--- tests/org/ast/tests/CollectionTypeTests.java
+++ tests/org/ast/tests/CollectionTypeTests.java
@@ -15,114 +15,106 @@
  */
 package org.ast.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.ast.tests.api.TestCollectionTypes;
 import org.ast.tests.base.HessianTest;
 import org.junit.Test;
 
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * {@link HessianTest} that is intended to test method calls with collection data types.
- * 
+ *
  * @author <a href="mailto:wolf@wolfpaulus.com">Wolf Paulus</a>
  */
 public class CollectionTypeTests extends HessianTest<TestCollectionTypes> {
-	
-	@SuppressWarnings("serial")
-	public CollectionTypeTests() throws Exception {
-		super(new TestCollectionTypes(){
-						
-			@Override
-			public List<Number> getList() {
-				List<Number> list = new ArrayList<Number>();
-				list.add(new Integer(1));
-				list.add(new Float(2));
-				list.add(new Double(3));
-				return list;
-			}
 
-			@Override
-			public Map<Integer, Date> getMap() {
-				Map<Integer,Date> map = new HashMap<Integer,Date>();				
-				map.put(1,new Date(1l));
-				map.put(2,new Date(2l));
-				map.put(3,new Date(3l));	
-				return map;
-			}
+    @SuppressWarnings("serial")
+    public CollectionTypeTests() throws Exception {
+        super(new TestCollectionTypes() {
 
-			@Override
-			public Set<String> getSet() {
-				Set<String> set = new HashSet<String>();
-				set.add("1");
-				set.add("2");
-				set.add("3");
-				return set;
-			}
-					
-			@Override
-			public Collection<Iterable<?>> getCollection() {
-				Collection<Iterable<?>> c = new ArrayList<Iterable<?>>();
-				c.add(getList());
-				c.add(getSet());
-				return c;
-			}			
-		});
-	}
-	
-	@Test public void callSet() throws Exception {
-		Set<String> set= client().getSet();	
-		int k=0;
-		for (String s : set) {
-			if ("1".equals(s)) k++;
-			if ("2".equals(s)) k++;
-			if ("3".equals(s)) k++;
-		}
-		assertEquals(3, k);
-	}
-	
-	@Test public void callList() throws Exception {		
-		Iterator<Number> it = client().getList().iterator();		
-		Number n = it.next();		
-		assertTrue(Integer.class.equals(n.getClass()));
-		assertTrue(new Integer(1).equals(1));
-		
-		n = it.next();
-		assertTrue(Double.class.equals(n.getClass()));
-		assertTrue(new Float(2).equals(2f));	
-		
-		n = it.next();
-		assertTrue(Double.class.equals(n.getClass()));
-		assertTrue(new Double(3).equals(3d));
-				
-		assertTrue(!it.hasNext());
-	}
-	
-	@Test public void callMap() throws Exception {		
-		Map<Integer,Date> map = client().getMap();
-		assertEquals(3,map.size());
-		assertEquals(new Date(1l), map.get(1));
-		assertEquals(new Date(2l), map.get(2));
-		assertEquals(new Date(3l), map.get(3));		
-	}
-	
-	@Test public void callCollection() throws Exception {	
-		Collection<Iterable<?>> c =client().getCollection();
-		Iterator<?> it = c.iterator();
-		Object obj = it.next();
-		assertTrue(ArrayList.class.equals(obj.getClass()));
-		obj = it.next();
-		assertTrue(HashSet.class.equals(obj.getClass()));
-		assertTrue(!it.hasNext());				
-	}
+            public List<Number> getList() {
+                List<Number> list = new ArrayList<Number>();
+                list.add(1);
+                list.add(2f);
+                list.add(3d);
+                return list;
+            }
+
+            public Map<Integer, Date> getMap() {
+                Map<Integer, Date> map = new HashMap<Integer, Date>();
+                map.put(1, new Date(1l));
+                map.put(2, new Date(2l));
+                map.put(3, new Date(3l));
+                return map;
+            }
+
+            public Set<String> getSet() {
+                Set<String> set = new HashSet<String>();
+                set.add("1");
+                set.add("2");
+                set.add("3");
+                return set;
+            }
+
+            public Collection<Iterable<?>> getCollection() {
+                Collection<Iterable<?>> c = new ArrayList<Iterable<?>>();
+                c.add(getList());
+                c.add(getSet());
+                return c;
+            }
+        });
+    }
+
+    @Test
+    public void callSet() throws Exception {
+        Set<String> set = client().getSet();
+        int k = 0;
+        for (String s : set) {
+            if ("1".equals(s)) k++;
+            if ("2".equals(s)) k++;
+            if ("3".equals(s)) k++;
+        }
+        assertEquals(3, k);
+    }
+
+    @Test
+    public void callList() throws Exception {
+        Iterator<Number> it = client().getList().iterator();
+        Number n = it.next();
+        assertTrue(Integer.class.equals(n.getClass()));
+        assertTrue(new Integer(1).equals(1));
+
+        n = it.next();
+        assertTrue(Double.class.equals(n.getClass()));
+        assertTrue(new Float(2).equals(2f));
+
+        n = it.next();
+        assertTrue(Double.class.equals(n.getClass()));
+        assertTrue(new Double(3).equals(3d));
+
+        assertTrue(!it.hasNext());
+    }
+
+    @Test
+    public void callMap() throws Exception {
+        Map<Integer, Date> map = client().getMap();
+        assertEquals(3, map.size());
+        assertEquals(new Date(1l), map.get(1));
+        assertEquals(new Date(2l), map.get(2));
+        assertEquals(new Date(3l), map.get(3));
+    }
+
+    @Test
+    public void callCollection() throws Exception {
+        Collection<Iterable<?>> c = client().getCollection();
+        Iterator<?> it = c.iterator();
+        Object obj = it.next();
+        assertTrue(ArrayList.class.equals(obj.getClass()));
+        obj = it.next();
+        assertTrue(HashSet.class.equals(obj.getClass()));
+        assertTrue(!it.hasNext());
+    }
 }
