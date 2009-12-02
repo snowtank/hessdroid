786745e0f65cab163248c51e1210a739541e33c6
wolf@wpbookpro.local <wolf@wpbookpro.local>
2009-12-01 16:17:02 -0800

    Fixed unittest and added sessionid field to cookie class for convenience

--- src/com/ast/util/CookieParser.java
+++ src/com/ast/util/CookieParser.java
@@ -18,51 +18,57 @@ package com.ast.util;
 /**
  * Parses Set-Cookie HTTP response header and returns the corresponding {@link Cookie} which can be
  * used for storing cookie information in java collections.
- * 
+ * <p/>
  * <a href="http://www.ietf.org/rfc/rfc2109">RFC 2109</a>
- * 
+ *
  * @author hessdroid@gmail.com
  */
 public class CookieParser {
 
-	/**
-	 * Abstract representation of an HTTP cookie.
-	 */
-	public static class Cookie {
-		
-		public String host;
-		public String value;
-		public String expires;
-		public String path;
-		public String domain;
-		public boolean secure;
-	}
-	
-	/**
-	 * Parses the given <tt>setCookieString</tt> from an HTTP response and creates a {@link Cookie} from it.
-	 * The {@link Cookie} can be used by clients to send cookie on subsequent requests.
-	 * 
-	 * @param host
-	 * @param setCookieString
-	 * 
-	 * @return a {@link Cookie} that was created from the given <tt>setCookieString</tt>
-	 */
-	public static Cookie parse(String host, String setCookieString)  {
-		
-		if (host == null) throw new IllegalArgumentException("Parameter \"host\" must not be null");
-		if (setCookieString == null) throw new IllegalArgumentException("Parameter \"setCookieString\" must not be null");
-		
-		Cookie result = new Cookie();
-		
-		String[] fields = setCookieString.split(";\\s*");
-	    
-		result.host = host;
-        result.value = fields[0];
+    /**
+     * Abstract representation of an HTTP cookie.
+     */
+    public static class Cookie {
+
+        public String host;
+        public String value; // cookie string without the leading intro: "Set-Cookie: "
+        public String expires;
+        public String path;
+        public String domain;
+        public String sessionId;
+        public boolean secure;
+    }
+
+    /**
+     * Parses the given <tt>setCookieString</tt> from an HTTP response and creates a {@link Cookie} from it.
+     * The {@link Cookie} can be used by clients to send cookie on subsequent requests.
+     *
+     * @param host <code>String</code> cookie host
+     * @param setCookieString <code>String</code> complete cookie string
+     * @return a {@link Cookie} that was created from the given <tt>setCookieString</tt>
+     */
+    public static Cookie parse(String host, String setCookieString) {
+
+        if (host == null) throw new IllegalArgumentException("Parameter \"host\" must not be null");
+        if (setCookieString == null)
+            throw new IllegalArgumentException("Parameter \"setCookieString\" must not be null");
+
+        Cookie result = new Cookie();
+
+        // split the cookie string on any semicolon or space(s)
+        String[] fields = setCookieString.split(";\\s*");
+        result.host = host;
+
+        // ignore leading cookie intro 
+        result.value = fields[0].startsWith("Set-Cookie: ") ? fields[0].substring(12) : fields[0];
+        if (result.value.startsWith("JSESSIONID=")) {
+            result.sessionId = result.value.substring(11);
+        }
 
         // Parse each field
-        for (int j=1; j<fields.length; j++) {
+        for (int j = 1; j < fields.length; j++) {
             if ("secure".equalsIgnoreCase(fields[j])) {
-            	result.secure = true;
+                result.secure = true;
             } else if (fields[j].indexOf('=') > 0) {
                 String[] f = fields[j].split("=");
                 if ("expires".equalsIgnoreCase(f[0])) {
@@ -74,8 +80,7 @@ public class CookieParser {
                 }
             }
         }
-		
-		return result;
-	}
-	
+        return result;
+    }
+
 }
--- tests/org/ast/tests/CookieParserTests.java
+++ tests/org/ast/tests/CookieParserTests.java
@@ -15,36 +15,78 @@
  */
 package org.ast.tests;
 
-import org.junit.Test;
-
 import com.ast.util.CookieParser;
 import com.ast.util.CookieParser.Cookie;
+import org.junit.Test;
 
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 public class CookieParserTests {
 
-	@Test public void testWithCookieValueAndPath() throws Exception {
-		
-		Cookie c = CookieParser.parse("myhost", "Set-Cookie: cookie-value; path=path");
-		
-		
-		assertEquals(c.host, "myhost");
-		assertEquals(c.path, "path");
-	}
-	
-	@Test public void testWithCompleteCookiePath() throws Exception {
-		// Set-Cookie: cookie-value; expires=date; path=path; domain=domain-name; secure
-		
-		Cookie c = CookieParser.parse("myhost", "Set-Cookie: cookie-value; expires=date; path=path; domain=domain-name; secure");
-		
-		
-		assertEquals(c.host, "myhost");
-		assertEquals(c.path, "path");
-		assertEquals(c.value, "cookie-value");
-		assertEquals(c.expires, "date");
-		assertEquals(c.domain, "domain");
-		assertEquals(c.secure, true);
-	}
-	
+    @Test
+    public void testWithCookieValueAndPath() throws Exception {
+
+        Cookie c = CookieParser.parse("myhost", "Set-Cookie: cookie-value; path=path");
+
+
+        assertEquals(c.host, "myhost");
+        assertEquals(c.path, "path");
+    }
+
+    @Test
+    public void testWithCompleteCookiePath() throws Exception {
+        // Set-Cookie: cookie-value; expires=date; path=path; domain=domain-name; secure
+
+        Cookie c = CookieParser.parse("myhost", "Set-Cookie: cookie-value; expires=date; path=path; domain=domain-name; secure");
+
+
+        assertEquals(c.host, "myhost");
+        assertEquals(c.path, "path");
+        assertEquals(c.value, "cookie-value");
+        assertEquals(c.expires, "date");
+        assertEquals(c.domain, "domain-name");
+        assertEquals(c.secure, true);
+    }
+
+    @Test
+    public void testWithRealWorldCookie() throws Exception {
+
+        // simple real-world cookie:
+        Cookie c = CookieParser.parse("myhost", "Set-Cookie: JSESSIONID=9126DE0DA91BD7D3C1A1A0608EC66645; Path=/Server");
+        assertEquals(c.path, "/Server");
+        assertEquals(c.value, "JSESSIONID=9126DE0DA91BD7D3C1A1A0608EC66645");
+        assertEquals(c.sessionId, "9126DE0DA91BD7D3C1A1A0608EC66645");
+    }
+
+    @Test
+    public void testEdgeCases() throws Exception {
+        Cookie c;
+        try {
+            c = CookieParser.parse("myhost", null);
+            assertEquals(c.host, "myhost"); // never executed
+        } catch (Exception e) {
+            assertTrue("Expect Exception to be thrown in CookieParser.parse()", true);
+        }
+        try {
+            c = CookieParser.parse(null, "Set-Cookie: cookie-value; path=path");
+            assertEquals(c.path, "path"); // never executed
+        } catch (Exception e) {
+            assertTrue("Expect Exception to be thrown in CookieParser.parse()", true);
+        }
+
+        try {
+            c = CookieParser.parse("myhost", "");
+            assertEquals(c.host, "myhost");
+            assertEquals(c.value,"");
+            c = CookieParser.parse("", "Set-Cookie: cookie-value; path=path");
+            assertEquals(c.host, "");            
+            assertEquals(c.value,"cookie-value");
+            c = CookieParser.parse("myhost", "0123456789");
+            assertEquals(c.value, "0123456789");
+        } catch (Exception e) {
+            assertTrue("Exception thrown in CookieParser.parse()", false);
+        }
+    }
+
 }
